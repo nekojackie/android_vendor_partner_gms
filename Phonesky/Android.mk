0494bc335afb15a25319998bbc64edceb8298e54
nekojackie <jackieawa@outlook.com>
2024-10-07 10:13:40 +0800

    Add PhoneSky

---
+++ Phonesky/Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp-permissions-com.android.vending.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := PhoneSky
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES=com.android.vending.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUIFFIX)
+LOCAL_CERTIFICATE := PRESIGEND
+LOCAL_REQUIRED_MODULES := privapp-permissions-com.android.vending.xml 
+include $(BUILD_PREBUILT)
